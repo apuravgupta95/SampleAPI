dcccc6cd49933e908f7a3c1549eba56ef9b9c222
apuravgupta95 <apuravgupta95@yahoo.com>
2018-07-06 23:05:34 +0530

    API ready with three endpoints

--- index.js
+++ index.js
@@ -1,8 +1,50 @@
 var express = require('express');
 var app = express();
 
-app.get('/api', (req,res) => {
-	res.send("Successfully entered API");
+var emails = [
+		'apuravgupta@merck.com',
+		'apurav@gmail.com',
+		'apurav',
+		'NA',
+		'ap@msd.com',
+		'',
+];
+
+var apps = [
+	'bgfbrgbrb',
+	'vfsbvdfbb',
+	'fsVEBVERB',
+	'frgevebvb',
+	'vefbvebae',
+	'vebvebabf',
+	'hrthrrttt',
+];
+
+var developers = [
+	'bgfbrgbrb',
+	'vfsbvdfbb',
+	'fsVEBVERB',
+	'frgevebvb',
+	'vefbvebae',
+	'vebvebabf',
+	'hrthrrttt',
+];
+
+app.get('/api/apps', (req,res) => {
+	
+	res.send(apps);
+	res.end();
+});
+
+app.get('/api/developers', (req,res) => {
+	
+	res.send(developers);
+	res.end();
+});
+
+app.get('/api/emails', (req,res) => {
+	
+	res.send(emails);
 	res.end();
 });
 
